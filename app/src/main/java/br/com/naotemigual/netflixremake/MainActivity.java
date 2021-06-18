354ec0e2c0e4e24cac34cef1afeb87176759ee89
Ricardo - Work <rickparreira@gmail.com>
2021-06-18 12:19:35 -0300

    net rolder

--- app/src/main/java/br/com/naotemigual/netflixremake/MainActivity.java
+++ app/src/main/java/br/com/naotemigual/netflixremake/MainActivity.java
@@ -8,11 +8,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.naotemigual.netflixremake.model.Category;
 import br.com.naotemigual.netflixremake.model.Movie;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,35 +24,97 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("Aventura"));
+        categories.add(new Category("Ação"));
+        categories.add(new Category("Comedia"));
+        categories.add(new Category("Romance"));
+        categories.add(new Category("Terror"));
+        categories.add(new Category("Documentario"));
+        categories.add(new Category("Teste 1 XYZ"));
+        categories.add(new Category("Teste 2 ABC"));
+        categories.add(new Category("Teste 3 EFG"));
+        categories.add(new Category("Teste 4 HIJ"));
 
-        // carregar lista de filmes falsa
-        List<Movie> movies = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            Movie movie = new Movie();
-            movie.setCoverUrl(i);
-            movies.add(movie);
+        for (Category category : categories) {
+
+             category.setMovies(new ArrayList<>());
+            for (int i = 0; i < 20; i++) {
+                Movie movie = new Movie();
+
+                if (i%2 == 0)
+                    movie.setCoverUrl(R.drawable.movie);
+                else
+                    movie.setCoverUrl(R.drawable.movie_4);
+
+                category.getMovies().add(movie);
+            }
         }
 
-        MainAdapter mainAdapter = new MainAdapter(movies);
+        MainAdapter mainAdapter = new MainAdapter(categories);
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
         recyclerView.setAdapter(mainAdapter);
     }
 
     private class MovieHolder extends RecyclerView.ViewHolder {
-        final TextView textViewUrl;
+        final ImageView imageView;
 
         public MovieHolder(@NonNull View itemView) {
             super(itemView);
-            textViewUrl = itemView.findViewById(R.id.text_view_url);
+            imageView = itemView.findViewById(R.id.image_view_cover);
         }
     }
 
-    private class MainAdapter extends RecyclerView.Adapter<MovieHolder> {
+    private class CategoryHolder extends RecyclerView.ViewHolder {
+        final TextView textViewTitle;
+        final RecyclerView recyclerViewMovie;
+
+        public CategoryHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            recyclerViewMovie = itemView.findViewById(R.id.recycler_view_movie);
+        }
+    }
+
+    private class MainAdapter extends RecyclerView.Adapter<CategoryHolder> {
+
+        private final List<Category> categories;
+
+        public MainAdapter(List<Category> categories) {
+            this.categories = categories;
+        }
+
+        @NonNull
+        @Override
+        public CategoryHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new CategoryHolder(getLayoutInflater().inflate(R.layout.category_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MainActivity.CategoryHolder holder, int position) {
+            Category category = categories.get(position);
+            holder.textViewTitle.setText(category.getName());
+
+            MovieAdapater movieAdapater = new MovieAdapater(category.getMovies());
+//            holder.recyclerViewMovie = findViewById(R.id.recycler_view);
+            holder.recyclerViewMovie.setAdapter(new MovieAdapater(category.getMovies()));
+
+            holder.recyclerViewMovie.setLayoutManager(new LinearLayoutManager(getBaseContext(), RecyclerView.HORIZONTAL, false));
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return categories.size();
+        }
+    }
+
+    private class MovieAdapater extends RecyclerView.Adapter<MovieHolder> {
 
         private final List<Movie> movies;
 
-        public MainAdapter(List<Movie> movies) {
+        public MovieAdapater(List<Movie> movies) {
             this.movies = movies;
         }
 
@@ -63,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull MainActivity.MovieHolder holder, int position) {
             Movie movie = movies.get(position);
-            holder.textViewUrl.setText(String.valueOf(movie.getCoverUrl()));
+            holder.imageView.setImageResource(movie.getCoverUrl());
         }
 
         @Override
--- app/src/main/java/br/com/naotemigual/netflixremake/model/Category.java
+++ app/src/main/java/br/com/naotemigual/netflixremake/model/Category.java
@@ -7,6 +7,10 @@ public class Category {
     private String name;
     private List<Movie> movies;
 
+    public Category(String name) {
+        this.name = name;
+    }
+
 
     public String getName() {
         return name;
